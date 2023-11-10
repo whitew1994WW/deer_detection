b1d8b23e4e7e5bba7af81f33c1608d5feab713bb
Will White <william.white@carfinance.co.uk>
2023-11-10 09:41:59 +0000

    Final commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # deer_detection
-For training a YOLO small model at detecting deer
+For training YOLO-tiny on detecting two classes:
+
+- Deer
+- Non-deer animal
+
+## Dataset
+The data set used is available on HuggingFace - "myyyyw/NTLNP", and can be downloaded as shown in the notebook.
+
+The dataset includes photos of deer and other animals at night and during the day. 
+
+The dataset annotations are in the Yolo format, but to make it easier to work with, I have converted it to the COCO format and created seperate annotation files for the train/test/val splits.
+
+## Training
+The model is trained using the Hugging Face API and the pytorch lightning module. See the train_deer notebook.
+
+The notebook is based on this notebook:
+
+https://colab.research.google.com/drive/1kfU7TpWYrZKRR8eGwdvKgpoEPUtC-J6I?ref=blog.roboflow.com#scrollTo=S2Pf11EvhWT5
+
+## Evaluation
+
+The model is evaluated using IoU, and achieves a 0.76 IoU on the test set. Given the small model size, this is not too bad for a proof of concept.
+
+## Video Annotation
+
+The video annotation is dont in the annotate_video.ipynb notebook. It will annotate a video with the bounding boxes at a given frequency, then save the output videos for review. 
---
+++ utils.py
@@ -0,0 +1,45 @@
+import torch
+import matplotlib.pyplot as plt
+
+# colors for visualization
+COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
+          [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
+
+# for output bounding box post-processing
+def box_cxcywh_to_xyxy(x):
+    x_c, y_c, w, h = x.unbind(1)
+    b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
+         (x_c + 0.5 * w), (y_c + 0.5 * h)]
+    return torch.stack(b, dim=1)
+
+def rescale_bboxes(out_bbox, size):
+    img_w, img_h = size
+    b = box_cxcywh_to_xyxy(out_bbox)
+    b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
+    return b
+
+def plot_results(pil_img, prob, boxes, id2label):
+    plt.figure(figsize=(16,10))
+    plt.imshow(pil_img)
+    ax = plt.gca()
+    colors = COLORS * 100
+    for p, (xmin, ymin, xmax, ymax), c in zip(prob, boxes.tolist(), colors):
+        ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                   fill=False, color=c, linewidth=3))
+        cl = p.argmax()
+        text = f'{id2label[cl.item()]}: {p[cl]:0.2f}'
+        ax.text(xmin, ymin, text, fontsize=15,
+                bbox=dict(facecolor='yellow', alpha=0.5))
+    plt.axis('off')
+    plt.show()
+
+def visualize_predictions(image, outputs, threshold=0.5):
+  # keep only predictions with confidence >= threshold
+  probas = outputs.logits.softmax(-1)[0, :, :-1]
+  keep = probas.max(-1).values > threshold
+  
+  # convert predicted boxes from [0; 1] to image scales
+  bboxes_scaled = rescale_bboxes(outputs.pred_boxes[0, keep].cpu(), image.size)
+
+  # plot results
+  plot_results(image, probas[keep], bboxes_scaled)
